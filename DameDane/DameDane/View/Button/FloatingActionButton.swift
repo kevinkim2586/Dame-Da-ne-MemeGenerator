c15d7c90a463654bc50c1b3129fae24a6dab7c03
Park <pjh00098@gmail.com>
2021-05-23 12:23:41 +0900

    add Floatting Action Button && Implement CompleteConvertScreen
    플로팅 액션 버튼 추가
    변환완료 화면 추가

---
+++ DameDane/DameDane/View/Button/FloatingActionButton.swift
@@ -0,0 +1,20 @@
+//
+//  FloatingActionButton.swift
+//  DameDane
+//
+//  Created by 박종호 on 2021/05/23.
+//
+
+import SwiftUI
+
+struct FloatingActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FloatingActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FloatingActionButton()
+    }
+}
